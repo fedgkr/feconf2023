d3f2d410ad20aa99cd77689bd9d7865e46edbcab
codemilli <hckrmoon@gmail.com>
2023-07-20 22:01:14 +0900

    update event date

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,4 @@
-export const EVENT_DATE_STR = '10월 28일';
+export const EVENT_DATE_STR = '10월 21일';
 export const CALL_FOR_SHARE_LINK =
   'https://forms.gle/SwonS8vkk29FCf79A?fbclid=IwAR0rqsOBO9KX0GRpG8x4JXKwbUUdhB9jojPjmaDjHfJO-P-b7pqXizTb21s';
 export const CALL_FOR_SPONSOR_LINK =
