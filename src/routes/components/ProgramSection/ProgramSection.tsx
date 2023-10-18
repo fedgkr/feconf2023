0c8d55bbaed5584cb6d2cda8645d622ae6940875
codemilli <hckrmoon@gmail.com>
2023-10-19 02:51:59 +0900

    Info section added

--- src/routes/components/ProgramSection/ProgramSection.tsx
+++ src/routes/components/ProgramSection/ProgramSection.tsx
@@ -1,7 +1,7 @@
 import { component$, $, useSignal } from '@builder.io/qwik';
 
 import css from './ProgramSection.module.scss';
-import { ProgramCard, TrackMenu } from './components';
+import { Info, ProgramCard, TrackMenu } from './components';
 import SectionIntro from '~/routes/components/SectionIntro';
 import map from 'lodash/map';
 import filter from 'lodash/filter';
@@ -26,6 +26,7 @@ const ProgramSection = component$(() => {
         title="FEConf 2023의 <br/>스피커와 프로그램을 소개합니다"
         description="국내 최대 프론트엔드 개발 컨퍼런스, FEConf는 2017년부터 프론트엔드 엔지니어의 다양한 도전과 경험을 주제로 프론트엔드 개발 커뮤니티가 서로 배우고, 함께 성장할 수 있는 생태계를 만들기 위해 노력하고 있습니다. 올해는 몰입(Flow)을 주제로 다양한 회사에서 활약하고 있는 프론트엔드 엔지니어들의 도전과 몰입, 그 과정에서 찾은 멋지고 흥미로운 이야기들을 준비했습니다. 10월 21일에 만나요!"
       />
+      <Info />
       <TrackMenu selected={currentTrack.value} onSelect={handleMenuSelect} />
       <div class={css.programList}>
         {map(filter(programs, filterFn), (program: Program, index) => (
--- src/routes/components/ProgramSection/components/index.ts
+++ src/routes/components/ProgramSection/components/index.ts
@@ -2,3 +2,4 @@ export { default as ProgramHeader } from './ProgramHeader';
 export { default as ProgramMobileHeader } from './ProgramMobileHeader';
 export { default as ProgramCard } from './ProgramCard';
 export { default as TrackMenu } from './TrackMenu';
+export { default as Info } from './Info';
